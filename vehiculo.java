826924895c99bde97823bc94a7e2a24e8df06c3c
yaresvc8 <yvilladiegoc@unicartagena.edu.co>
2025-09-13 16:33:43 -0500

    ejercicio 2 taller 6 (parte 1)

--- vehiculo.java
+++ vehiculo.java
@@ -1,5 +1,16 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+package vehiculos;
+
+public class vehiculo {
+    protected String tipo;
+    protected String marca;
+
+    public vehiculo(String tipo, String marca) {
+        this.tipo = tipo;
+        this.marca = marca;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Marca: " + marca);
     }
 }
